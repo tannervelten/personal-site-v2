0fe3079374df0c6c3d1d37da77876826c05ac906
Tanner Velten <tannervelten@gmail.com>
2018-10-25 20:04:25 -0700

    Comments out unecessary code in NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
 
-const styles = {
-};
+// const styles = {
+// };
 
 function NotFound(props) {
-  const { classes } = props;
+  // const { classes } = props;
 
   return (
     <div>Oops!</div>
   );
 }
 
-NotFound.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+// NotFound.propTypes = {
+//   classes: PropTypes.object.isRequired,
+// };
 
-export default withStyles(styles)(NotFound);
+// export default withStyles(styles)(NotFound);
+export default NotFound;
